b12c2cf26a6c12361ad8cc731ef41e6687f70f00
thiagopls1 <thiagopls1@hotmail.com>
2025-05-25 16:36:46 -0300

    build: add `jest`, `firebase` and `dotenv` and it's configurations

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env.development.local',
+});
+
+const jestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  testTimeout: 60000,
+  verbose: true,
+  transform: {},
+};
+
+export default jestConfig;
