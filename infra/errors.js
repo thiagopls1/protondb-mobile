963b504060e7b92f97771b0bd44d14ced5879be6
thiagopls1 <thiagopls1@hotmail.com>
2025-05-27 21:37:02 -0300

    feat: add new exceptions

--- infra/errors.js
+++ infra/errors.js
@@ -27,11 +27,53 @@ export class ServiceError extends Error {
 }
 
 export class BadRequestError extends Error {
-  constructor(message, action) {
-    super(message || 'Os dados recebidos estão inválidos');
+  constructor(message, action, cause) {
+    super(message || 'Os dados recebidos estão inválidos', {
+      cause: cause,
+    });
     this.name = 'BadRequestError';
     this.action = action || 'Verifique se os dados enviados estão corretos';
     this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class UnauthorizedError extends Error {
+  constructor(message, action, cause) {
+    super(message || 'Usuário não autenticado', {
+      cause: cause,
+    });
+    this.name = 'UnauthorizedError';
+    this.action = action || 'Realize a autenticação';
+    this.statusCode = 401;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ConflictError extends Error {
+  constructor(message, action, cause) {
+    super(message || 'Recurso já existente', {
+      cause: cause,
+    });
+    this.name = 'ConflictError';
+    this.action = action || 'Crie um recurso que não exista ainda';
+    this.statusCode = 409;
   }
 
   toJSON() {
